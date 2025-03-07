5ec4521cfe8a66d29bc5eba239c3f96cfd31e5ee
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2025-03-08 00:50:55 +0500

    Add files via upload

---
+++ womenday.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("🌸 Happy Women's Day! 🌸")
+
+st.image("temp_image_20250308_004059_bdbc30d9-bf75-4df9-adc8-ad02421005b2.jpg", caption="sabahat")
+
+st.markdown(
+    """
+    ## 💖 **A Tribute to Women Everywhere**
+    
+    On this special day, let's take a moment to appreciate the achievements and contributions of women in every aspect of life. 
+    
+    - 🧠 **Wisdom** and **creativity** that inspire the world.
+    - 💪 **Strength** and **courage** that uplift communities.
+    - 💖 **Kindness** and **compassion** that make the world a better place.
+    
+    Take a moment to reflect, celebrate, and spread love. 🌸
+    
+    """
+)
+
+st.info("Made with love by Taha Qureshi 💜")
